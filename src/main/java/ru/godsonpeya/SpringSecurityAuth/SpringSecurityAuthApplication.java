dfffee1f2289fa1e1dd09e6ab6ca823d8d598181
peyagodson <e.peya-bamba@sport-express.ru>
2019-12-23 18:19:58 +0300

    first commit

---
+++ src/main/java/ru/godsonpeya/SpringSecurityAuth/SpringSecurityAuthApplication.java
@@ -0,0 +1,13 @@
+package ru.godsonpeya.SpringSecurityAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/godsonpeya/SpringSecurityAuth/SpringSecurityAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.godsonpeya.SpringSecurityAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
